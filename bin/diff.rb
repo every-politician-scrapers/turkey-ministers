a77b862f33ae5b3bdb296b2fceda1cf4ec5ff3ed
Tony Bowden <tony@tmtm.com>
2021-08-25 19:58:19 +0300

    Skip President
    As President, Erdoğan doesn't appear on the list of ministers, so this
    is an expected omission.

--- bin/diff.rb
+++ bin/diff.rb
@@ -3,5 +3,11 @@
 
 require 'every_politician_scraper/comparison'
 
+# Not listed on the site
+SKIP = [
+  ['---', 'Recep Tayyip Erdoğan', 'President of Turkey']
+].freeze
+
 diff = EveryPoliticianScraper::Comparison.new('data/wikidata.csv', 'data/official.csv').diff
+                                         .reject { |row| SKIP.include? row }
 puts diff.sort_by { |r| [r.first, r[1].to_s] }.reverse.map(&:to_csv)
