1aa76237fd1ffc8de85a98094c6d5ae315a3b715
Tony Bowden <tony@tmtm.com>
2022-07-15 17:19:04 +0300

    move official comparison to sources/

--- sources/official/diff.rb
+++ sources/official/diff.rb
@@ -8,6 +8,6 @@ SKIP = [
   ['---', 'Recep Tayyip Erdoğan', 'President of Turkey']
 ].freeze
 
-diff = EveryPoliticianScraper::DecoratedComparison.new('data/wikidata.csv', 'data/official.csv').diff
+diff = EveryPoliticianScraper::DecoratedComparison.new('wikidata.csv', 'scraped.csv').diff
                                          .reject { |row| SKIP.include? row }
 puts diff.sort_by { |r| [r.first, r[1].to_s] }.reverse.map(&:to_csv)
